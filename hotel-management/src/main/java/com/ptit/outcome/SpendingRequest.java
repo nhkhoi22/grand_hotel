74d71bde99527fe9f12a54e18ebe5a5463626d3a
nhkhoi22 <nhkhoi22@gmail.com>
2019-10-14 08:54:50 +0700

    add product

---
+++ hotel-management/src/main/java/com/ptit/outcome/SpendingRequest.java
@@ -0,0 +1,22 @@
+//package com.ptit.outcome;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import javax.persistence.Table;
+//
+//import lombok.Data;
+//
+//@Data
+//@Entity
+//@Table(name = "spending_request")
+//public class SpendingRequest {
+//	
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	@Column(name = "request_id", columnDefinition = "BIGINT")
+//	private Long id;
+//	
+//}
---
+++ hotel-management/src/main/java/com/ptit/product/ProductType.java
@@ -0,0 +1,62 @@
+package com.ptit.product;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.ptit.customer.Service;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "product_type")
+public class ProductType {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "product_type_id", columnDefinition = "INT")
+	private int id;
+	
+	@Column(name = "product_type_name", columnDefinition = "VARCHAR(100)")
+	private String name;
+	
+	@OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY,
+            mappedBy = "productType"
+    )
+	private List<Service> services;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Service> getServices() {
+		return services;
+	}
+
+	public void setServices(List<Service> services) {
+		this.services = services;
+	}
+}
