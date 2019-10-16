83031f87754908a119a751ecc13a670c4f083139
Nakajima-Kuro <tnnamanh@gmail.com>
2019-10-16 15:16:45 +0700

    a

--- hotel-management/src/main/java/com/ptit/controller/GErrorController.java
+++ hotel-management/src/main/java/com/ptit/controller/GErrorController.java
@@ -1,24 +1,48 @@
 package com.ptit.controller;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-public class ErrorController {
+public class GErrorController implements ErrorController {
 	
-	@RequestMapping(value = { "/errors/404" }, method = RequestMethod.GET)
 	public ModelAndView errorNotFound() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("errors/404");
 		return modelAndView;
 	}
-
-	@RequestMapping(value = { "/errors/500" }, method = RequestMethod.GET)
+	
 	public ModelAndView errorNotAvailable() {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("errors/500");
 		return modelAndView;
+	}
+
+	@RequestMapping("/error")
+	public ModelAndView handleError(HttpServletRequest request) {
+	    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+	    if (status != null) {
+	        Integer statusCode = Integer.valueOf(status.toString());
+	     
+	        if(statusCode == HttpStatus.NOT_FOUND.value()) {
+	        	return errorNotFound();
+	        }
+	        else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+	        	return errorNotAvailable();
+	        }
+	    }
+		return null;
+	}
+
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
