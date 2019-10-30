897d39c127d9942bad0882a8064de0ae4d1ac345
Nakajima-Kuro <tnnamanh@gmail.com>
2019-10-30 22:14:45 +0700

    Add name to each rows

--- hotel-management/src/main/resources/static/js/user-common/request-form.js
+++ hotel-management/src/main/resources/static/js/user-common/request-form.js
@@ -1,6 +1,10 @@
 function AddToTable() {
     var product = document.getElementById("product").value;
+    var productID = document.getElementById("product").id;
     var quanity = document.getElementById("quanity").value;
+    if (isNaN(quanity)) {
+        return;
+    }
     var table = document.getElementById("requestTable");
     var row = table.insertRow(0);
     row.classList.add("row");
@@ -10,11 +14,14 @@ function AddToTable() {
     cell1.classList.add("col-sm-5");
     cell2.classList.add("col-sm-5");
     cell3.classList.add("col-sm-2");
-    cell1.setAttribute("value", product);
+    cell1.setAttribute("value", productID);
     cell1.setAttribute("value", quanity);
     cell1.innerHTML = product;
     cell2.innerHTML = quanity;
     cell3.innerHTML = '<input class="btn btn-block btn-danger" type="button" value="Delete" onclick="deleteRow(this)"/>';
+    for (let i = 0; i < rows.length; i++) {
+        row.name = "product" + table.rows[i];
+    }
     document.getElementById("quanity").value = "";
 }
 function deleteRow(btn) {
