ae081eaaded72ee37a3a1d5eeb8650b8f2d94e38
nhkhoi22 <nhkhoi22@gmail.com>
2019-11-03 00:07:07 +0700

    fix

--- hotel-management/src/main/java/com/ptit/product/PriceHistory.java
+++ hotel-management/src/main/java/com/ptit/product/PriceHistory.java
@@ -25,7 +25,6 @@ public class PriceHistory implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	@Id
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "out_product_id", columnDefinition = "INT")
 	private OutProduct service;
